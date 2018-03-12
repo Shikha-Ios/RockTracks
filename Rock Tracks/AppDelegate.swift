26d0fa53a6cd081c32b16299808b79cc09b63ce9
shikha <shikha@shikhas-MacBook-Air.local>
2018-03-12 13:52:55 +0000

    changes in target

--- Rock Tracks/AppDelegate.swift
+++ Rock Tracks/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
         let BarButtonItemAppearance = UIBarButtonItem.appearance()
         BarButtonItemAppearance.setTitleTextAttributes([NSAttributedStringKey.foregroundColor: UIColor.clear], for: .normal)
         return true
